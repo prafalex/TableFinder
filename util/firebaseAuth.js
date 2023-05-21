1b1f858b120d0da2b2a03043369b518dcbbf0bc0
prafalex <andrei.praf@s.unibuc.ro>
2023-05-21 18:22:16 +0300

    update api key

--- util/firebaseAuth.js
+++ util/firebaseAuth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY='AIzaSyAY9Y2wYuCXRC52CcxrQ5c7gWJX7XSqI68';
+const API_KEY=////;
 
 export async function UserCreate(email,password){
     const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
